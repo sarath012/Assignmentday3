d1a37e8ee432f7dcb0df6eb73d3c7dfc89a68768
Test <Test@TM-DESKTOP18.TECHMOJO.LOCAL>
2022-07-25 15:43:57 +0530

    compltd

--- day3/src/Components/Products.js
+++ day3/src/Components/Products.js
@@ -3,14 +3,14 @@ import products from '../Objects/products.json'
 
 export default function Products() {
     const [sampleProducts, setSampleProducts] = useState(products)
-    const [titleFilter, setTitleFilter] = useState('');
-    const [categoryFilter, setCategoryFilter] = useState('');
+    // const [titleFilter, setTitleFilter] = useState('');
+    // const [categoryFilter, setCategoryFilter] = useState('');
     const [titleAsc, setTitleAsc] = useState(true);
     const [categoryAsc, setCategoryAsc] = useState(true);
 
     useEffect(()=>{
         handleTitleSortAsc()
-    },[])
+    },[]);
 
     function handleTitleSortAsc(){
         const titleSortedAsc = [...sampleProducts].sort((a,b)=>a.title > b.title ? 1 : -1)
@@ -35,30 +35,34 @@ export default function Products() {
         setCategoryAsc(!categoryAsc)
     }
 
-    const productrows = [...sampleProducts]
-        // .filter((product) => ((product.title.toLowerCase()).includes(titleFilter) || titleFilter === ''))
-        // .filter((product) => ((product.category.toLowerCase()).includes(categoryFilter) || categoryFilter === ''))
-        .map((product) =>
-            <tr key={product.id}>
-                <td>{product.title}</td>
-                <td>{product.category}</td>
-                <td>{product.price}</td>
-                <td>{product.rating.rate}</td>
-            </tr>
-            
-    );
+    function searchCategory(searchValue) {
+        const categoryFilt = products.filter((product) => ((product.category.toLowerCase()).includes(searchValue) || searchValue === ''))
+        setSampleProducts(categoryFilt)
+    }
+        
+    function searchTitle(searchValue) {
+        const titleFilt = products.filter((product) => ((product.title.toLowerCase()).includes(searchValue) || searchValue === ''))
+        setSampleProducts(titleFilt)
+    }
+
+        const productrows = sampleProducts.map((product) =>
+                                <tr key={product.id}>
+                                    <td>{product.title}</td>
+                                    <td>{product.category}</td>
+                                    <td>{product.price}</td>
+                                    <td>{product.rating.rate}</td>
+                                </tr>
+                            );
+    
 
     const submit =()=>{
-        const titles=products
-        .filter((product) => ((product.title.toLowerCase()).includes(titleFilter) || titleFilter === ''))
-        .filter((product) => ((product.category.toLowerCase()).includes(categoryFilter) || categoryFilter === ''))
-        .map((product) => console.log(product.title));
+                    sampleProducts.map((product) =>console.log(product.title))
     }
 
   return (
     <div className='producttable'>
-      <input type='text' placeholder="title"  value={titleFilter} onChange={e => setTitleFilter(e.target.value.toLowerCase())}/>
-      <input type='text' placeholder="category"  value={categoryFilter} onChange={e => setCategoryFilter(e.target.value.toLowerCase())}/>
+      <input type='text' placeholder="title"  onChange={e => searchTitle(e.target.value.toLowerCase())}/>
+      <input type='text' placeholder="category" onChange={e => searchCategory(e.target.value.toLowerCase())}/>
       <table>
             <thead>
                 <tr>
